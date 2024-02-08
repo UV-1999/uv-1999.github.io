ca3d8aad62415bd47918121680f00cc60ddbaa18
Piyush Marmat <80977229+UV-1999@users.noreply.github.com>
2024-02-09 00:45:24 +0530

    Update header.php

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <title><?= isset($PageTitle) ? $PageTitle : "Default Title"?></title>
+    <title><?= isset($PageTitle) ? $PageTitle : "Header testing"?></title>
     <!-- Additional tags here -->
     <?php if (function_exists('customPageHeader')){
       customPageHeader();
