513afd302d8262df6684e41ae462b73ce0969ff3
Piyush Marmat <80977229+UV-1999@users.noreply.github.com>
2024-02-08 20:36:18 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,16 @@
+<header>
+       <nav>
+            <ul style="list-style: none;padding: 0;margin: 0;">
+                <strong>
+                <li style="font-size: 36px; display: inline; margin-right: 20px;"><a href="/">Piyush Marmat</a></li>
+                <br>
+                <hr>
+                <li style="font-size: 20px; display: inline; margin-right: 20px; text-align:center;"><a href="/cv.html">CV</a></li>
+                <li style="font-size: 20px; display: inline; margin-right: 20px; text-align:center;"><a href="/research.html">Research</a></li>
+                <li style="font-size: 20px; display: inline; margin-right: 20px; text-align:center;"><a href="/extra.html">Extra</a></li>
+                </strong>
+            </ul>
+        </nav>
+        
+    <button class="dark-mode-button" id="darkmodebutton">☀️</button>
+    </header>
