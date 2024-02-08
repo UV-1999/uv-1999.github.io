43a96658e8ed9e93c9dd7a7a26b1ae261e27582d
Piyush Marmat <80977229+UV-1999@users.noreply.github.com>
2024-02-08 23:48:12 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,17 +1,11 @@
-<?php
-       echo <header>;
-       echo <nav>;
-       echo <ul style="list-style: none;padding: 0;margin: 0;">;
-       echo <strong>;
-       echo <li style="font-size: 36px; display: inline; margin-right: 20px;"><a href="/">Piyush Marmat</a></li>;
-       echo <br>;
-       echo <hr>;
-       echo <li style="font-size: 20px; display: inline; margin-right: 20px; text-align:center;"><a href="/cv.html">CV</a></li>;
-       echo <li style="font-size: 20px; display: inline; margin-right: 20px; text-align:center;"><a href="/research.html">Research</a></li>;
-       echo <li style="font-size: 20px; display: inline; margin-right: 20px; text-align:center;"><a href="/extra.html">Extra</a></li>;
-       echo </strong>;
-       echo </ul>;
-       echo </nav>;
-       echo <button class="dark-mode-button" id="darkmodebutton">☀️</button>;
-       echo </header>;
-?>
+<!doctype html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title><?= isset($PageTitle) ? $PageTitle : "Default Title"?></title>
+    <!-- Additional tags here -->
+    <?php if (function_exists('customPageHeader')){
+      customPageHeader();
+    }?>
+  </head>
+  <body>
