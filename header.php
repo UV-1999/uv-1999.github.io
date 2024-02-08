11218cd4aca3afe69626fd51c8152d63d427ce63
Piyush Marmat <80977229+UV-1999@users.noreply.github.com>
2024-02-08 21:06:21 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,17 +1,17 @@
-<!doctype html>
-<header>
-       <nav>
-            <ul style="list-style: none;padding: 0;margin: 0;">
-                <strong>
-                       <li style="font-size: 36px; display: inline; margin-right: 20px;"><a href="/">Piyush Marmat</a></li>
-                       <br>
-                       <hr>
-                       <li style="font-size: 20px; display: inline; margin-right: 20px; text-align:center;"><a href="/cv.html">CV</a></li>
-                       <li style="font-size: 20px; display: inline; margin-right: 20px; text-align:center;"><a href="/research.html">Research</a></li>
-                       <li style="font-size: 20px; display: inline; margin-right: 20px; text-align:center;"><a href="/extra.html">Extra</a></li>
-                </strong>
-            </ul>
-        </nav>
-        
-    <button class="dark-mode-button" id="darkmodebutton">☀️</button>
-</header>
+<?php
+       echo <header>;
+       echo <nav>;
+       echo <ul style="list-style: none;padding: 0;margin: 0;">;
+       echo <strong>;
+       echo <li style="font-size: 36px; display: inline; margin-right: 20px;"><a href="/">Piyush Marmat</a></li>;
+       echo <br>;
+       echo <hr>;
+       echo <li style="font-size: 20px; display: inline; margin-right: 20px; text-align:center;"><a href="/cv.html">CV</a></li>;
+       echo <li style="font-size: 20px; display: inline; margin-right: 20px; text-align:center;"><a href="/research.html">Research</a></li>;
+       echo <li style="font-size: 20px; display: inline; margin-right: 20px; text-align:center;"><a href="/extra.html">Extra</a></li>;
+       echo </strong>;
+       echo </ul>;
+       echo </nav>;
+       echo <button class="dark-mode-button" id="darkmodebutton">☀️</button>;
+       echo </header>;
+?>
