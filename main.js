f53884b858fede81be2945103e4b75336989cbdf
Piyush Marmat <80977229+UV-1999@users.noreply.github.com>
2024-02-10 01:38:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-    fetch('footer.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('footer-placeholder').innerHTML = html;
-        });
-
+    
     const button = document.getElementById("darkmodebutton");
     
     function toggleDarkMode() {
