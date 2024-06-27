93d6f4a692dcfcf4754727ed36c262a2bfc4b3f4
Piyush Marmat <80977229+UV-1999@users.noreply.github.com>
2024-06-27 18:50:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,66 +1,59 @@
-    
-    const button = document.getElementById("darkmodebutton");
-    
-    function toggleDarkMode() {
-        const element = document.querySelector('body');
-        element.classList.add('dark-mode');
-        const head = document.querySelector('header');
-        head.classList.add('dark-mode');
-        const ae = document.querySelectorAll('a');
-        ae.forEach(anchor => {
+const button = document.getElementById("darkmodebutton");
+
+function toggleDarkMode() {
+    const element = document.querySelector('body');
+    element.classList.add('dark-mode');
+    const head = document.querySelector('header');
+    head.classList.add('dark-mode');
+    const ae = document.querySelectorAll('a');
+    ae.forEach(anchor => {
         anchor.classList.add('dark-mode');
-        });
-        const he = document.querySelectorAll('hr');
-        he.forEach(hline => {
+    });
+    const he = document.querySelectorAll('hr');
+    he.forEach(hline => {
         hline.classList.add('dark-mode');
-        });
-        const icon = document.querySelector('icon-link');
-        icon.classList.add('dark-mode');
+    });
+}
 
-        
-        
-    }
-
-    function toggleLightMode() {
-        const element = document.querySelector('body');
-        element.classList.remove('dark-mode');
-        const head = document.querySelector('header');
-        head.classList.remove('dark-mode');
-        const ae = document.querySelectorAll('a');
-        ae.forEach(anchor => {
+function toggleLightMode() {
+    const element = document.querySelector('body');
+    element.classList.remove('dark-mode');
+    const head = document.querySelector('header');
+    head.classList.remove('dark-mode');
+    const ae = document.querySelectorAll('a');
+    ae.forEach(anchor => {
         anchor.classList.remove('dark-mode');
-        });
-        const he = document.querySelectorAll('hr');
-        he.forEach(hline => {
+    });
+    const he = document.querySelectorAll('hr');
+    he.forEach(hline => {
         hline.classList.remove('dark-mode');
-        });
-        const icon = document.querySelector('icon-link');
-        icon.classList.remove('dark-mode');
+    });
+}
 
-   
-        
-    }
-    
-    button.addEventListener("click", function () {
-        if (button.textContent == "☀️") {
-            button.textContent = "🌙";
-            localStorage.setItem('preferred-theme', 'dark');
-            toggleDarkMode();
-            
-        } else if  (button.textContent == "🌙") {
-            button.textContent = "☀️";
-            localStorage.setItem('preferred-theme', 'light');
-            toggleLightMode();
-            
-        }    });
-    const userPreference = localStorage.getItem('preferred-theme')
-    if (userPreference === 'dark') {
-        
-        button.textContent = "🌙"; // Enable dark mode
+button.addEventListener("click", function () {
+    const icon = button.querySelector('i');
+    if (icon.classList.contains('fa-sun')) {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+        localStorage.setItem('preferred-theme', 'dark');
         toggleDarkMode();
-      } else if (userPreference === 'light'){
-        
-        button.textContent = "☀️";
-
+    } else if (icon.classList.contains('fa-moon')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+        localStorage.setItem('preferred-theme', 'light');
         toggleLightMode();
     }
+});
+
+const userPreference = localStorage.getItem('preferred-theme');
+if (userPreference === 'dark') {
+    const icon = button.querySelector('i');
+    icon.classList.remove('fa-sun');
+    icon.classList.add('fa-moon');
+    toggleDarkMode();
+} else if (userPreference === 'light') {
+    const icon = button.querySelector('i');
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun');
+    toggleLightMode();
+}
